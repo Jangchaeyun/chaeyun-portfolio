e5671bad52fc385ab315a54f941b523ac0a49dab
zzangsally <62280276+Jangchaeyun@users.noreply.github.com>
2022-12-01 15:33:46 +0900

    Header Section

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
 
 import './About.scss';
 
+const abouts = [
+  { title: '웹 개발자', descpition: '저는 좋은 웹개발자가 되고 싶습니다.', imgUrl: '' },
+  { title: '프론트엔드 개발자', descpition: '저는 좋은 프론트엔드 개발자가 되고 싶습니다.', imgUrl: '' }
+];
+
 const About = () => {
   return (
-    <div>
-      About
-    </div>
+    <>
+      <h2 className='head-text'>저는 
+        <span> 프론트엔드 개발자</span>가 되고 싶습니다.
+        <br />
+        <span>REACT</span>
+      </h2>
+
+      <div className='app__profiles'>
+        {abouts.map((about, index) => (
+          <motion.div
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.6, type: 'tween' }}
+          >
+
+          </motion.div>
+        ))}
+      </div>
+    </>
   );
 }
 
--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -1,11 +1,71 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 
+
+import { images } from '../../constants';
 import './Header.scss';
+
+const scaleVariants = {
+  whileInView: {
+    scale: [0, 1],
+    opacity: [0, 1],
+    transition:  {
+      duration: 1,
+      ease: 'easeInOut'
+    }
+  }
+}
 
 const Header = () => {
   return (
-    <div>
-      Header
+    <div id='home' className='app__header app__flex'>
+      <motion.div
+        whileInView={{ x: [ -100, 0 ], opacity: [0, 1] }}
+        transition={{ duration: 5 }}
+        className="app__header-info"
+      >
+        <div className='app__header-badge'>
+          <div className="badge-cmp app__flex">
+            <span>👋</span>
+            <div style={{ marginLeft : 20 }}>
+              <p className='p-text'>안녕하세요</p>
+              <h1 className='head-text'> 장채윤의 </h1>
+              <h1 className='head-text'> 포토폴리오</h1>
+              <p className='p-text'>에 오신 것을 환영합니다.</p>
+            </div>
+          </div>
+
+          <div className="tag-cmp app__flex">
+            <p className='p-text'>프론트엔드 개발자</p>
+            <p className='p-text'>부천대학교 대학생</p>            
+          </div>
+        </div>
+      </motion.div>
+
+      <motion.div
+        whileInView={{ opacity: [0, 1] }}
+        transition={{ duration: 0.5, delayChildren: 0.5 }}
+        className="app__header-img"
+      >
+        <motion.img
+          whileInView={{ scale: [0, 1] }}
+          transition={{ duration: 1, ease: 'easeInOut' }}
+          src={images.circle}
+          alt="profile"
+          className="overlay circle"
+        />
+      </motion.div>
+      <motion.div
+        variant={scaleVariants}
+        whileInView={scaleVariants.whileInView}
+        className="app__header-circles"
+      >
+        {[images.node, images.react, images.css].map((circle, index) => (
+          <div className='circle-cmp app__flex' key={`circle-${index}`}>
+            <img src={circle} alt="circle" />
+          </div>
+        ))}
+      </motion.div>
     </div>
   );
 }
