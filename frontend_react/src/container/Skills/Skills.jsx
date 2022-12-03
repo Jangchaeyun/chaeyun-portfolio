2a6657a83df310faa6b34281c45b7fa50e0498f6
zzangsally <62280276+Jangchaeyun@users.noreply.github.com>
2022-12-03 21:10:09 +0900

    Skills Section

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -25,7 +25,7 @@ const Skills = () => {
 
   return (
     <>
-      <h2 className="head-text">Skills & Experiences</h2>
+      <h2 className="head-text">스킬 & 경험</h2>
 
       <div className="app__skills-container">
         <motion.div className="app__skills-list">
