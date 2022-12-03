d5bb03be08eea9da51dd90fc7fabde1ddd865e4d
zzangsally <62280276+Jangchaeyun@users.noreply.github.com>
2022-12-03 16:39:26 +0900

    Skills Section

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 
-import { AppWrap } from '../../wrapper';
-
 import './About.scss';
 import { urlFor, client } from '../../client';
+import { AppWrap, MotionWrap } from '../../wrapper';
 
 const About = () => {
 
@@ -26,18 +25,18 @@ const About = () => {
         <span>프론트엔드 개발자</span>가 되고 싶습니다.
       </h2>
 
-      <div className="app__profiles">
+      <div className='app__profiles'>
         {abouts.map((about, index) => (
           <motion.div
             whileInView={{ opacity: 1 }}
             whileHover={{ scale: 1.1 }}
-            transition={{ duration: 0.5, type: 'tween' }}
-            className="app__profile-item"
+            transition={{ duration: 0.6, type: 'tween' }}
+            className='app__profile-item'
             key={about.title + index}
           >
             <img src={urlFor(about.imgUrl)} alt={about.title} />
-            <h2 className="bold-text" style={{ marginTop: 20 }}>{about.title}</h2>
-            <p className="p-text" style={{ marginTop: 10 }}>{about.description}</p>
+            <h2 className='bold-text' style={{ margin: 20 }}>{about.title}</h2>
+            <p className='p-text' style={{ margin: 10 }}>{about.description}</p>
           </motion.div>
         ))}
       </div>
@@ -45,4 +44,8 @@ const About = () => {
   );
 }
 
-export default AppWrap(About, '대해서');
+export default AppWrap(
+  MotionWrap(About, 'app__about'),
+  '대해서',
+  'app__whitebg',
+);
