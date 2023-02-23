41d2974ec575269ce9642ed3c5ab4ce784aeb9d9
zzangsally <62280276+Jangchaeyun@users.noreply.github.com>
2023-02-23 13:48:21 +0900

    텍스트 변경

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -1,42 +1,41 @@
-import React from 'react';
-import { motion } from 'framer-motion';
+import React from "react";
+import { motion } from "framer-motion";
 
-import { AppWrap } from '../../wrapper';
-import { images } from '../../constants';
-import './Header.scss';
+import { AppWrap } from "../../wrapper";
+import { images } from "../../constants";
+import "./Header.scss";
 
 const scaleVariants = {
   whileInView: {
     scale: [0, 1],
     opacity: [0, 1],
-    transition:  {
+    transition: {
       duration: 1,
-      ease: 'easeInOut'
-    }
-  }
-}
+      ease: "easeInOut",
+    },
+  },
+};
 
 const Header = () => {
   return (
-    <div className='app__header app__flex'>
+    <div className="app__header app__flex">
       <motion.div
-        whileInView={{ x: [ -100, 0 ], opacity: [0, 1] }}
+        whileInView={{ x: [-100, 0], opacity: [0, 1] }}
         transition={{ duration: 5 }}
-        className="app__header-info"
-      >
-        <div className='app__header-badge'>
+        className="app__header-info">
+        <div className="app__header-badge">
           <div className="badge-cmp app__flex">
             <span>👋</span>
-            <div style={{ marginLeft : 20 }}>
-              <p className='p-text'>안녕하세요</p>
-              <h1 className='head-text'> 장채윤의 </h1>
-              <h1 className='head-text'> 포토폴리오</h1>
-              <p className='p-text'>에 오신 것을 환영합니다.</p>
+            <div style={{ marginLeft: 20 }}>
+              <p className="p-text">안녕하세요</p>
+              <h1 className="head-text"> 장채윤의 </h1>
+              <h1 className="head-text"> 포토폴리오</h1>
+              <p className="p-text">에 오신 것을 환영합니다.</p>
             </div>
           </div>
 
           <div className="tag-cmp app__flex">
-            <p className='p-text'>부천대학교 재학생</p>            
+            <p className="p-text">부천대학교 졸업생</p>
           </div>
         </div>
       </motion.div>
@@ -44,12 +43,11 @@ const Header = () => {
       <motion.div
         whileInView={{ opacity: [0, 1] }}
         transition={{ duration: 0.5, delayChildren: 0.5 }}
-        className="app__header-img"
-      >
-        <img src={images.profile} alt="profile_bg" className='profile' />
+        className="app__header-img">
+        <img src={images.profile} alt="profile_bg" className="profile" />
         <motion.img
           whileInView={{ scale: [0, 1] }}
-          transition={{ duration: 1, ease: 'easeInOut' }}
+          transition={{ duration: 1, ease: "easeInOut" }}
           src={images.circle}
           alt="profile_circle"
           className="overlay_circle"
@@ -59,16 +57,15 @@ const Header = () => {
       <motion.div
         variant={scaleVariants}
         whileInView={scaleVariants.whileInView}
-        className="app__header-circles"
-      >
+        className="app__header-circles">
         {[images.node, images.react, images.css].map((circle, index) => (
-          <div className='circle-cmp app__flex' key={`circle-${index}`}>
+          <div className="circle-cmp app__flex" key={`circle-${index}`}>
             <img src={circle} alt="circle" />
           </div>
         ))}
       </motion.div>
     </div>
   );
-}
+};
 
-export default AppWrap(Header, '홈');
+export default AppWrap(Header, "홈");
